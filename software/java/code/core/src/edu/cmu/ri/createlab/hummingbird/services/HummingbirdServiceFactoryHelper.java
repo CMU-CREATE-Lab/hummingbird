4d272b2b004e5852677351a61b806b5217ec6e68
bartley@cmu.edu <bartley@cmu.edu@8f9ae9e0-bd35-6d31-192c-db6722d48ff7>
2011-09-22 20:18:57 +0000

    Changes required to due change in BaseAudioServiceImpl.  Can now easily specify the directory containing audio files for when expressions are executed.

---
+++ software/java/code/core/src/edu/cmu/ri/createlab/hummingbird/services/HummingbirdServiceFactoryHelper.java
@@ -0,0 +1,15 @@
+package edu.cmu.ri.createlab.hummingbird.services;
+
+import java.io.File;
+
+/**
+ * <p>
+ * <code>HummingbirdServiceFactoryHelper</code> helps the {@link HummingbirdServiceFactory} create services.
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public interface HummingbirdServiceFactoryHelper
+   {
+   File getAudioDirectory();
+   }
